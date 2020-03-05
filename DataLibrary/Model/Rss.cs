94b0b7ba537c373f14457d47739c4d0c1637dc3c
Tomasz Zając <zajact19@gmail.com>
2020-03-05 13:56:31 +0100

    Adding Rss class

---
+++ DataLibrary/Model/Rss.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataLibrary.Model
+{
+	[XmlRoot(ElementName = "rss")]
+	public class Rss
+	{
+		public Channel Channel { get; set; }
+		[XmlAttribute(AttributeName = "version")]
+		public string Version { get; set; }
+	}
+}
