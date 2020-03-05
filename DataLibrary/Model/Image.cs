116eb85567c086348fe8b21696ad9ccc7d18c08d
Tomasz Zając <zajact19@gmail.com>
2020-03-05 13:53:51 +0100

    Adding Image class

---
+++ DataLibrary/Model/Image.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataLibrary.Model
+{
+	[XmlRoot(ElementName = "image")]
+	public class Image
+	{
+		[XmlElement(ElementName = "title")]
+		public string Title { get; set; }
+		[XmlElement(ElementName = "url")]
+		public string Url { get; set; }
+		[XmlElement(ElementName = "link")]
+		public string Link { get; set; }
+		[XmlElement(ElementName = "width")]
+		public string Width { get; set; }
+		[XmlElement(ElementName = "height")]
+		public string Height { get; set; }
+	}
+}
