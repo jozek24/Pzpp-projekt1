a775f5379ae85fd6ea1bd99341b8a09bf6901b10
Józef Salik <jozek243@gmail.com>
2020-03-18 13:24:10 +0100

    add Rss class

---
+++ MediaReviewer/Model/Rss.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MediaReviewer.Model
+{
+    class Rss
+    {
+       // [BsonElement("channel")]
+      //  public Channel Channel { get; set; }
+
+        [BsonElement("version")]
+        public string Version { get; set; }
+
+    }
+}
