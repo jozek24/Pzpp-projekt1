6221ef9fe96fc47347ee2f564fe472f1bc97715b
Józef Salik <jozek243@gmail.com>
2020-03-18 13:26:59 +0100

    changes in Item class

--- MediaReviewer/Model/Item.cs
+++ MediaReviewer/Model/Item.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MediaReviewer.Model
 {
     class Item
     {
+        [BsonElement("title")]
         public string Title { get; set; }
+        [BsonElement("link")]
         public string Link { get; set; }
+        [BsonElement("comments")]
         public string Comments { get; set; }
+        [BsonElement("guid")]
         public Guid Guid { get; set; }
+        [BsonElement("category")]
         public List<string> Category { get; set; }
+        [BsonElement("description")]
         public string Description { get; set; }
+        [BsonElement("pubDate")]
         public string PubDate { get; set; }
     }
 }
