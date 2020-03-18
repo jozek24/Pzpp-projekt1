e680a4df04d6d5882d291396c234f3bf78f66977
Józef Salik <jozek243@gmail.com>
2020-03-18 13:35:54 +0100

    change moddifier to public in 'Rss' and 'Item' classes

--- MediaReviewer/Model/Item.cs
+++ MediaReviewer/Model/Item.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace MediaReviewer.Model
 {
-    class Item
+    public class Item
     {
         [BsonElement("title")]
         public string Title { get; set; }
