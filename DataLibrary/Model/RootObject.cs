ede0f10cf9c5c9308e839f4f892d15bba6cbf8aa
Tomasz Zając <zajact19@gmail.com>
2020-03-19 10:33:26 +0100

    Adding RootObject class

---
+++ DataLibrary/Model/RootObject.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Model
+{
+	public class RootObject
+	{
+		[JsonProperty("__invalid_name__?xml")]
+		public Xml __invalid_name__xml { get; set; }
+		public Rss rss { get; set; }
+	}
+}
