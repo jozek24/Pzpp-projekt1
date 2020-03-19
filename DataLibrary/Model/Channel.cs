fd184963f53007a4826a3eee665fe7f1ddc0b6ce
Tomasz Zając <zajact19@gmail.com>
2020-03-19 10:20:43 +0100

    Changing Channel class

--- DataLibrary/Model/Channel.cs
+++ DataLibrary/Model/Channel.cs
@@ -7,28 +7,17 @@ using System.Xml.Serialization;
 
 namespace DataLibrary.Model
 {
-	[XmlRoot(ElementName = "channel")]
 	public class Channel
 	{
-		[XmlElement(ElementName = "title")]
 		public string Title { get; set; }
-		[XmlElement(ElementName = "link")]
 		public string Link { get; set; }
-		[XmlElement(ElementName = "description")]
 		public string Description { get; set; }
-		[XmlElement(ElementName = "language")]
 		public string Language { get; set; }
-		[XmlElement(ElementName = "pubDate")]
 		public string PubDate { get; set; }
-		[XmlElement(ElementName = "webMaster")]
 		public string WebMaster { get; set; }
-		[XmlElement(ElementName = "docs")]
 		public string Docs { get; set; }
-		[XmlElement(ElementName = "ttl")]
 		public string Ttl { get; set; }
-		[XmlElement(ElementName = "image")]
 		public Image Image { get; set; }
-		[XmlElement(ElementName = "item")]
 		public List<Item> Item { get; set; }
 	}
 }
