e8c6444261354b139a657c58005b7b016b670b6b
Tomasz Zając <zajact19@gmail.com>
2020-03-19 10:27:43 +0100

    Changing Item class

--- DataLibrary/Model/Item.cs
+++ DataLibrary/Model/Item.cs
@@ -7,22 +7,14 @@ using System.Xml.Serialization;
 
 namespace DataLibrary.Model
 {
-	[XmlRoot(ElementName = "item")]
 	public class Item
 	{
-		[XmlElement(ElementName = "title")]
 		public string Title { get; set; }
-		[XmlElement(ElementName = "link")]
 		public string Link { get; set; }
-		[XmlElement(ElementName = "comments")]
 		public string Comments { get; set; }
-		[XmlElement(ElementName = "guid")]
 		public Guid Guid { get; set; }
-		[XmlElement(ElementName = "category")]
 		public List<string> Category { get; set; }
-		[XmlElement(ElementName = "description")]
-		public string Description { get; set; }
-		[XmlElement(ElementName = "pubDate")]
+		public Description Description { get; set; }
 		public string PubDate { get; set; }
 	}
 }
