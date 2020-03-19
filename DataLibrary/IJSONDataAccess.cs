1503c259f0bbed575c2ed0e14972b701ffb1bbac
Tomasz Zając <zajact19@gmail.com>
2020-03-19 11:36:04 +0100

    Adding  DeserializeJSONToList method in IJSONDataAccess

--- DataLibrary/IJSONDataAccess.cs
+++ DataLibrary/IJSONDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataLibrary.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,7 @@ namespace DataLibrary
     {
         string SerializeXmlToJSON(string xml);
         string DeserializeJSON(string json);
+        List<RootObject> DeserializeJSONToList(List<string> json);
+
     }
 }
