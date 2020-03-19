f50e456decaaeb79b67c33678a16664b3fb85cac
Tomasz Zając <zajact19@gmail.com>
2020-03-19 10:30:38 +0100

    Adding Description class

---
+++ DataLibrary/Model/Description.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Model
+{
+	public class Description
+	{
+		[JsonProperty("__invalid_name__#cdata-section")]
+		public string __invalid_name__cdata_section { get; set; }
+	}
+}
