a9b38c9d994ab65c965e56e7ca4d5cf62213be3a
Tomasz Zając <zajact19@gmail.com>
2020-03-19 10:32:07 +0100

    Adding Xml class

---
+++ DataLibrary/Model/Xml.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Model
+{
+	public class Xml
+	{
+		[JsonProperty("__invalid_name__@version")]
+		public string __invalid_name__version { get; set; }
+		[JsonProperty("__invalid_name__@encoding")]
+		public string __invalid_name__encoding { get; set; }
+	}
+}
