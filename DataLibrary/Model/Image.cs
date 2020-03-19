8a6c3ac22c9a600d20c1650dccdc057670817903
Tomasz Zając <zajact19@gmail.com>
2020-03-19 10:26:45 +0100

    Changing Image class

--- DataLibrary/Model/Image.cs
+++ DataLibrary/Model/Image.cs
@@ -7,18 +7,12 @@ using System.Xml.Serialization;
 
 namespace DataLibrary.Model
 {
-	[XmlRoot(ElementName = "image")]
 	public class Image
 	{
-		[XmlElement(ElementName = "title")]
 		public string Title { get; set; }
-		[XmlElement(ElementName = "url")]
 		public string Url { get; set; }
-		[XmlElement(ElementName = "link")]
 		public string Link { get; set; }
-		[XmlElement(ElementName = "width")]
 		public string Width { get; set; }
-		[XmlElement(ElementName = "height")]
 		public string Height { get; set; }
 	}
 }
