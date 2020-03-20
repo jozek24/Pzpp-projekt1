1fc1edec4cec39a49465622a0dde330fa4b96fdf
Józef Salik <jozek243@gmail.com>
2020-03-20 18:02:20 +0100

    add class RootObject

---
+++ MediaReviewer/Model/RootObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaReviewer.Model
+{
+    public class RootObject
+    {
+        public Xml Xml { get; set; }
+        public Rss Rss { get; set; }
+    }
+}
