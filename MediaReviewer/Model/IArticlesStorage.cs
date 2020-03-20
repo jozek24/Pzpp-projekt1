68b11c77b36b6fbac029c65ad219bf00790058a6
Józef Salik <jozek243@gmail.com>
2020-03-20 18:40:00 +0100

    add interface IArticlesStorage

---
+++ MediaReviewer/Model/IArticlesStorage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MediaReviewer.Model
+{
+    public interface IArticlesStorage
+    {
+        /// <summary>
+        /// This method give you a List of passed objects from database.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="table"></param>
+        /// <returns></returns>
+        List<T> LoadRecords<T>(string table);
+    }
+}
