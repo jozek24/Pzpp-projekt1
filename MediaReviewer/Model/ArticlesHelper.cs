5fd8c5902335316b89f3b624af7fbc0ae08eeb20
Józef Salik <jozek243@gmail.com>
2020-03-20 18:43:33 +0100

    add class 'ArticlesHelper' with method GetArticles()

---
+++ MediaReviewer/Model/ArticlesHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace MediaReviewer.Model
+{
+    public class ArticlesHelper
+    {
+        private IArticlesStorage _articlesStorage;
+        public ArticlesHelper(string databaseName, IArticlesStorage articlesStorage = null)
+        {
+            _articlesStorage = articlesStorage ?? new ArticlesStorage(databaseName);
+        }
+
+        public List<RootObject> GetArticles()
+        {
+            var rootObject =  _articlesStorage.LoadRecords<RootObject>("RootObject");
+
+            return rootObject;
+        }
+    }
+}
