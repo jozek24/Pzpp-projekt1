993b1427b37728538f2b62af3b45e4847f07b456
Józef Salik <jozek243@gmail.com>
2020-03-20 18:41:39 +0100

    add class 'ArticlesStorage' with method 'LoadRecords' to provide get tables from database

---
+++ MediaReviewer/Model/ArticlesStorage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MediaReviewer.Model
+{
+    public class ArticlesStorage : IArticlesStorage
+    {
+        private IMongoDatabase db;
+        public ArticlesStorage(string databaseName)
+        {
+            var client = new MongoClient();
+            db = client.GetDatabase(databaseName);
+        }
+        public List<T> LoadRecords<T>(string table)
+        {
+            var collection = db.GetCollection<T>(table);
+            return collection.Find(new BsonDocument()).ToList();
+        }
+    }
+}
