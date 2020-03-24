19f7a4983209e9078ed9313102c795d0ef59369b
Tomasz Zając <zajact19@gmail.com>
2020-03-24 12:42:41 +0100

    Changing IJSONDataAccess

--- DataLibrary/IJSONDataAccess.cs
+++ DataLibrary/IJSONDataAccess.cs
@@ -9,10 +9,7 @@ namespace DataLibrary
 {
     public interface IJSONDataAccess
     {
-        string SerializeXmlToJSON(string xml);
-        string DeserializeJSON(string json);
         List<string> SerializeXMLToJSONlist(List<string> xml);
         List<RootObject> DeserializeJSONToList(List<string> json);
-
     }
 }
