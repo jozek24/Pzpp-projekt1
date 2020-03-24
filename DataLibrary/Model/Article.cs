b67a0090c729159ce84536c2d9ad25632e8de717
Tomasz Zając <zajact19@gmail.com>
2020-03-24 12:37:53 +0100

    Adding Article class

---
+++ DataLibrary/Model/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Model
+{
+    public class Article
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+
+        public string HTML { get; set; }
+
+        public List<string> Category { get; set; }
+
+        public string PubDate { get; set; }
+    }
+}
