5e547c281b2d6b15c24828f65c70fdfdf35e40f0
lukno <lukasz1081@wp.pl>
2020-03-24 18:04:57 +0100

    Add class MongoCRUD with methods

---
+++ DataLibrary/MongoCRUD.cs
@@ -0,0 +1,33 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary
+{
+    class MongoCRUD
+    {
+		private IMongoDatabase db;
+
+		public MongoCRUD(string database) //chyba połączenie do bazy
+		{
+			var client = new MongoClient();
+			db = client.GetDatabase(database);
+		}
+		public void InsertRecord<T>(string table, List<T> record)
+		{
+			var collection = db.GetCollection<T>(table);//tworzenie kolekcji gengerycznej
+			collection.InsertMany(record); //wstawia wiele rekord
+		}
+		public List<T> LoadRecords<T>(string table)
+		{
+			var collection = db.GetCollection<T>(table);
+			return collection.Find(new BsonDocument()).ToList();
+		}
+
+	}
+}
