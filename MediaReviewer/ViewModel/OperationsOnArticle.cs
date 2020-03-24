182356cbe1599de9fb04e7e4721a631b2ba8e927
KonGarli <garlaczkonrad@gmail.com>
2020-03-24 18:12:21 +0100

    Add Interface View

---
+++ MediaReviewer/ViewModel/OperationsOnArticle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MediaReviewer.ViewModel
+{
+    class OperationsOnArticle: Article
+    {
+        void AddArtField()
+        {
+
+        }
+
+        void AddButton()
+        {
+          
+        }
+    }
+}
