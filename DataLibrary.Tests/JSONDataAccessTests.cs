de3d629ae42815d5e6b4629652359815814d39f5
Tomasz Zając <zajact19@gmail.com>
2020-03-25 21:19:41 +0100

    Adding new class for tests

---
+++ DataLibrary.Tests/JSONDataAccessTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Tests
+{
+    public class JSONDataAccessTests
+    {
+        private IJSONDataAccess _JSONDataAccess = new JSONDataAccess();
+    }
+}
---
+++ DataLibrary.Tests/XMLDataAccessTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Xunit;
+
+namespace DataLibrary.Tests
+{
+    public class XMLDataAccessTests
+    {
+        private IHTMLDataAccess _HTMLDataAccess = new HTMLDataAccess();
+        private IXMLDataAccess _XMLDataAccess = new XMLDataAccess();
+
+        [Fact]
+        public void GetListOfXmlDocument_ValidUrls()
+        {
+            //ToDo
+            _HTMLDataAccess.GetHTML("");
+            List<XmlDocument> actual = _XMLDataAccess.GetListOfXmlDocument(_HTMLDataAccess.ListOfXMLURL);
+            Assert.True(actual.Count > 0);
+        }
+    }
+}
