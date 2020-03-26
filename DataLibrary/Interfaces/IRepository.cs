bf0d2072288b5871ef146d2db432f5d3720f874e
lukno <lukasz1081@wp.pl>
2020-03-26 09:32:45 +0100

    add interface IRepository

---
+++ DataLibrary/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DataLibrary.Interfaces
+{
+    interface IRepository
+    {
+        void CheckNewData();
+        void OnTimedEvent(Object source, ElapsedEventArgs e);
+        void AddNewRssChannels(List<RssChannel> rssChannels);
+    }
+}
